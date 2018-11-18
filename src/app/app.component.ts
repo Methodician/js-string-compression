c353a75ab7acce4eb071ba9e232b92eb978c0a2f
Methodician <jacob@flight.run>
2018-11-18 12:22:01 -0800

    installed major packages and tested ng serve

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'txc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'txt-compress';
+}
