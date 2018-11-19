f29796e84c1a402836d7b606667afd9c305dd40c
Methodician <jacob@flight.run>
2018-11-19 12:46:41 -0800

    The beginnings of a working model.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,51 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const onWriteCompressed = functions.database.ref('compressed/{name}')
+    .onWrite((change, context) => {
+        if (!change.after.exists) {
+            // do nothing if it was deleted or empty
+            return null;
+        }
+
+        const newString = change.after.val();
+        const naturalRef = change.after.ref.parent.parent.child('natural').child(context.params.name);
+        return naturalRef.set(newString);
+    });
+
+const compressText = (text: string): string => {
+
+    const maxIndex = text.length - 1;
+    let matchLength = 15;
+    let matchString = text.slice(0, matchLength);
+    let index = 15;
+    while (index !== -1 && matchLength > 5) {
+        index = text.indexOf(matchString)
+        if (index === -1) {
+            index = index + 1;
+        } else {
+            text.replace(matchString, `<${index},${matchLength}>`);
+            // Could skip a few indecies here based on the length of the encoded string
+        }
+        if (index >= maxIndex - matchLength) {
+            matchLength--;
+            index = matchLength;
+        }
+    }
+    return text;
+
+}
+
+export const onWriteNatural = functions.database.ref('natural/{name}')
+    .onWrite((change, context) => {
+        if (!change.after.exists) {
+            // do nothing if it was deleted or empty
+            return null;
+        }
+
+        const oldString = change.after.val();
+        const newString = compressText(oldString);
+        const compressedRef = change.after.ref.parent.parent.child('compressed').child(context.params.name);
+        return compressedRef.set(newString);
+    });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import * as fb from 'firebase/app';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  compressedItem: string;
-  naturalItem: string;
+  compressed: string;
+  natural: string;
   name: string;
   keyList: Observable<any[]>;
   serverTimestamp = fb.database.ServerValue.TIMESTAMP;
@@ -27,11 +27,14 @@ export class AppComponent {
     this.db.object<string>(`compressed/${name}`)
       .valueChanges()
       .subscribe(val => {
-        this.compressedItem = val;
+        console.log('compressed', val);
+        this.compressed = val;
       });
     this.db.object<string>(`natural/${name}`)
-      .valueChanges().subscribe(val => {
-        this.naturalItem = val;
+      .valueChanges()
+      .subscribe(val => {
+        console.log('natural', val);
+        this.natural = val;
       });
   }
 
@@ -40,12 +43,48 @@ export class AppComponent {
       alert('We can\'t set an item without a title');
       return;
     }
-    if (!this.naturalItem || this.naturalItem.length < 40) {
+    if (!this.natural || this.natural.length < 40) {
       alert('this is kind of pointless. Compress something longer.');
       return;
     }
-    this.db.object(`natural/${this.name}`).set(this.naturalItem);
-    this.db.object(`keys/${this.name}`).set(this.serverTimestamp);
+    this.compressText();
+    // this.db.object(`compressed/${this.name}`).set(compressedItem);
+    // this.selectItem(this.name);
+    // this.db.object(`natural/${this.name}`).set(this.naturalItem);
+    // this.db.object(`keys/${this.name}`).set(this.serverTimestamp);
+  }
+
+
+  compressText = () => {
+    let matchString = '';
+    let bookmark = 14;
+    let text = this.natural;
+    for (let matchLength = 35; matchLength > 8; matchLength--)
+      for (let increment = 0; increment < text.length - 1; increment++) {
+        if (increment > text.length - matchLength) {
+          // Avoid checking the last few characters
+          continue;
+        }
+        matchString = text.slice(increment, increment + matchLength);
+        bookmark = text.indexOf(matchString, bookmark);
+        if (bookmark === -1) {
+          // No match found, increment forward with new index.
+          bookmark = increment + matchLength;
+          continue
+        } else {
+          // Match found at bookmark index.
+          // console.log(`FOUND "${matchString}" at index ${bookmark}`);
+          const pointer = `<${increment},${matchLength}>`;
+          // console.log('pointer: ' + pointer);
+          const regExp = new RegExp(matchString, 'g');
+          const head = text.slice(0, bookmark);
+          const tail = text.slice(bookmark, text.length + 1).replace(regExp, pointer);
+          // text = text.replace(regExp, pointer);
+          text = head + tail;
+          // console.log('TEXT:', text);
+        }
+      }
+    this.compressed = text;
   }
 
   decompress() {
@@ -53,13 +92,13 @@ export class AppComponent {
       alert('We can\'t set an item without a title');
       return;
     }
-    if (!this.compressedItem || this.compressedItem.length < 8) {
+    if (!this.compressed || this.compressed.length < 8) {
       alert('That\'s pretty short. I doubt it\'s even compressed.');
       return;
     }
-    this.db.object(`compressed/${this.name}`).set(this.compressedItem);
+    this.selectItem(this.name);
+    this.db.object(`compressed/${this.name}`).set(this.compressed);
     this.db.object(`keys/${this.name}`).set(this.serverTimestamp);
   }
-
 }
 
